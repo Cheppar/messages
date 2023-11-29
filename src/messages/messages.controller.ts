f218653d42062ce83f42f3ef18dc8d28441c0744
Cheppar <ejioforched@gmail.com>
2023-11-29 15:34:30 +0300

    feat: dto create-message-dto

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message-dto';
 
-@Controller('/messages')
+@Controller('messages')
 export class MessagesController {
     @Get()
     listMessages() {
@@ -8,13 +9,13 @@ export class MessagesController {
     }
 
     @Post()
-    createMessage() {
-
+    createMessage(@Body() body: CreateMessageDto) {
+        console.log(body)
     }
 
     @Get('/:id')
-    getMessage() {
-
+    getMessage(@Param('id') id: string) {
+        console.log(id)
     }
 
 }
