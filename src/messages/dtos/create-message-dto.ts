fee37162d7d036f4bf336c921d99b7cfa0c2453c
Cheppar <ejioforched@gmail.com>
2023-11-30 11:05:10 +0300

    feat: Service and Repository

--- src/messages/dtos/create-message-dto.ts
+++ src/messages/dtos/create-message-dto.ts
@@ -2,6 +2,7 @@ import { IsString } from 'class-validator';
 
 export class CreateMessageDto {
 
+    // validation rule: ensure content in the body is 'string'
     @IsString()
     content: string
 }
